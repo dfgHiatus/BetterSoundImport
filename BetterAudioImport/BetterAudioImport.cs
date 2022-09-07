d0e182846a37f382f772b2fa1e3ee48ba9034b12
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-09-07 12:17:37 -0500

    big bang

---
+++ BetterAudioImport/BetterAudioImport.cs
@@ -0,0 +1,102 @@
+﻿using BaseX;
+using CodeX;
+using FrooxEngine;
+using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Threading.Tasks;
+
+namespace ModNameGoesHere
+{
+    public class BetterAudioImport : NeosMod
+    {
+        public override string Name => "BetterAudioImport";
+        public override string Author => "dfgHiatus";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/dfgHiatus/BetterSoundImport";
+        private static ModConfiguration config;
+
+        [AutoRegisterConfigKey]
+		private static ModConfigurationKey<AudioTypes> audioType =
+			new("audioType",
+			"Format to convert all new audio to.",
+			() => AudioTypes.OGG);
+
+		public override void OnEngineInit()
+        {
+            new Harmony("net.dfgHiatus.BetterAudioImport").PatchAll();
+			config = GetConfiguration();
+		}
+
+		private static Uri uriCopy;
+		
+        [HarmonyPatch(typeof(AudioPlayerOrb), "OnAttach")]
+        class AudioPlayerOrbPatch
+        {
+            public static void Postfix(AudioPlayerOrb __instance)
+            {
+				AudioEncodeSettings settings = new VorbisEncodeSettings();
+				switch (config.GetValue(audioType))
+				{
+					case AudioTypes.OGG:
+						settings = new VorbisEncodeSettings();
+						break;
+					case AudioTypes.FLAC:
+						settings = new FlacEncodeSettings();
+						break;
+					case AudioTypes.WAV:
+						settings = new WavEncodeSettings();
+						break;
+					default:
+						settings = new VorbisEncodeSettings();
+						break;
+				}
+
+				// TODO Wait until the audio is loaded
+
+				if (__instance.AudioClip.Asset.Data.EncodeSettings != settings)
+                {
+					var uriField = __instance.Slot.GetComponent<StaticAudioClip>().URL.Value;
+					Process((AudioX a) => a, __instance.AudioClip.Asset, uriField, settings);
+					uriField = uriCopy;
+				}
+			}
+
+            private static void Process(Func<AudioX, AudioX> processFunc, AudioClip clip, Uri uriField, AudioEncodeSettings encodeSettings)
+            {
+				Engine.Current.WorldManager.FocusedWorld.Coroutines.StartTask(async delegate
+				{
+					await ProcessAsync(processFunc, clip, uriField, encodeSettings);
+				});
+			}
+
+			private static async Task ProcessAsync(Func<AudioX, AudioX> processFunc, AudioClip clip, Uri uriField, AudioEncodeSettings encodeSettings = null)
+			{
+				if (uriField == null)
+					return;
+
+				while (clip == null)
+					await default(NextUpdate);
+
+				Uri uri;
+
+				try
+				{
+					AudioX audio = processFunc(await clip.GetOriginalAudioData().ConfigureAwait(continueOnCapturedContext: false));
+					uri = await Engine.Current.LocalDB.SaveAssetAsync(audio, encodeSettings).ConfigureAwait(continueOnCapturedContext: false);
+				}
+				catch (Exception ex)
+				{
+					UniLog.Error($"Exception processing audio clip {uriField}:\n" + ex);
+					await default(ToWorld);
+					throw;
+				}
+
+				await default(ToWorld);
+
+				if (!(uri == null))
+					uriCopy = uri;
+			}
+		}
+    }
+}
--- BetterAudioImport/Properties/AssemblyInfo.cs
+++ BetterAudioImport/Properties/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModNameGoesHere")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BetterSoundImporter")]
+[assembly: AssemblyDescription("Convert audio files on import to NeosVR.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModNameGoesHere")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCompany("Hiatus Labs")]
+[assembly: AssemblyProduct("BetterSoundImporter")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
